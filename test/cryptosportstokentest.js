e4ec2c97022a07ab9bd22884820cb53554ffaf46
marcpre <marcus.presich@gmail.com>
2018-02-06 20:46:52 +0100

    trying to fix cryptosportstoken

--- test/cryptosportstokentest.js
+++ test/cryptosportstokentest.js
@@ -13,7 +13,7 @@ contract('CryptoSportsToken', function (accounts) {
       cryptoSportsToken = instance;
       return cryptoSportsToken.createPromoPerson(accounts[1], "string _name", 100)
     }).then(function (result) {
-      console.log(result.logs[0].event)
+      console.log(result.logs[0].event) 
     })
   })
   
@@ -44,50 +44,27 @@ contract('CryptoSportsToken', function (accounts) {
   });
   
   it("should let users buy if they send enough eth", function() {
-    let KH;
-    let watcher;
+    let cst;
+    let watcherSold;
+    let watcherBirth;
     let idx;
-    return KetherHomepage.new(owner, withdrawWallet)
+    return CryptoSportsToken.new(ceo, coo)
       .then(function(instance) {
-        KH = instance;
-        watcher = KH.Buy();
+        cst = instance;
+        cst.createPromoPerson(accounts[1], "string _name", 100)
 
-        return KH.buy(0, 0, 10, 10, { value: oneHundredCellPrice, from: account1 })
+        watcherSold = cst.TokenSold();
+        watcherBirth = cst.Birth();
+        console.log("watcherBirth")
+        console.log(watcherBirth)
+        return watcherBirth;
       })
       .then(function(result) {
-        // Make sure we issued the right Buy() event
-        assert.equal("Buy", result.logs[0].event);
-        const buyEvent = result.logs[0].args;
+        
+        console.log(result.logs[0].event)
 
-        idx = buyEvent.idx;
-
-        assert.equal(account1, buyEvent.owner)
-        assert.equal(0, buyEvent.x.toNumber());
-        assert.equal(0, buyEvent.y.toNumber());
-        assert.equal(10, buyEvent.width.toNumber());
-        assert.equal(10, buyEvent.height.toNumber());
-
-        // make sure the grid is filled
-        return KH.grid.call(0, 0);
       })
-      .then(function(res) {
-        assert.equal(true, res);
-
-        return KH.grid.call(10, 10);
-      })
-      .then(function(res) {
-        assert.equal(false, res);
-
-        return KH.ads.call(idx);
-      })
-      .then(function(ad) {
-        // Make sure we added the ad
-        assert.equal(account1, ad[0]);
-        assert.equal(0, ad[1].toNumber());
-        assert.equal(0, ad[2].toNumber());
-        assert.equal(10, ad[3].toNumber());
-        assert.equal(10, ad[4].toNumber());
-      })
+      
   });
   
 });
